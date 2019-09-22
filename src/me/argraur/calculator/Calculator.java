dfc301839545a0e503b4f8e9e4a06ccc28856aea
argraur <argraur@gmail.com>
2019-09-22 11:53:51 +0300

    Merge symb and dec arrays into unified calc double array

--- src/me/argraur/calculator/Calculator.java
+++ src/me/argraur/calculator/Calculator.java
@@ -1,7 +1,9 @@
 package me.argraur.calculator;
 
 class Calculator extends GUI {
-    private String[] dec = new String[10], symb = new String[10];
+    // String[integer][0]
+    // String[symbol][1]
+    private String[][] calc = new String[10][2];
     private int curr = 0;
 
     private Calculator() {
@@ -26,45 +28,45 @@ class Calculator extends GUI {
         plButton.addActionListener(actionEvent -> addSymbol("+"));
         eqButton.addActionListener(actionEvent -> result());
 
-        for (int i = 0; i < 10; i++) {
-            dec[i] = ""; symb[i] = "";
-        }
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 2; j++)
+                calc[i][j] = "";
     }
 
     private void addNumber(int a) {
         addText(String.valueOf(a));
-        dec[curr] = dec[curr] + a;
+        calc[curr][0] = calc[curr][0] + a;
     }
 
     private void addSymbol(String symb) {
         // Check if we don't place 2 symbols in a row
-        try { Integer.parseInt(dec[curr]); } catch (NumberFormatException e) { return; }
+        try { Integer.parseInt(calc[curr][0]); } catch (NumberFormatException e) { return; }
         // Add symbol to the textBox
         addText(" " + symb + " ");
-        this.symb[curr] = symb;
+        calc[curr][1] = symb;
         curr++;
     }
 
     private void addZero() {
-        if (!dec[curr].equals("") || curr != 0) addNumber(0);
+        if (!calc[curr][0].equals("") || curr != 0) addNumber(0);
     }
 
     private void result() {
-        float res = 0, frs = Float.parseFloat(dec[0]);
+        float res = 0, frs = Float.parseFloat(calc[curr][0]);
         for (int i = 0; i < curr; i++) {
-            switch (symb[i]) {
+            switch (calc[i][1]) {
                 case "/":
-                    if (Float.parseFloat(dec[i + 1]) == 0) { errorZero(); return; }
-                    res = frs / Float.parseFloat(dec[i + 1]);
+                    if (Float.parseFloat(calc[i + 1][0]) == 0) { errorZero(); return; }
+                    res = frs / Float.parseFloat(calc[i + 1][0]);
                     break;
                 case "*":
-                    res = frs * Float.parseFloat(dec[i + 1]);
+                    res = frs * Float.parseFloat(calc[i + 1][0]);
                     break;
                 case "-":
-                    res = frs - Float.parseFloat(dec[i + 1]);
+                    res = frs - Float.parseFloat(calc[i + 1][0]);
                     break;
                 case "+":
-                    res = frs + Float.parseFloat(dec[i + 1]);
+                    res = frs + Float.parseFloat(calc[i + 1][0]);
                     break;
             }
             frs = res;
@@ -75,9 +77,9 @@ class Calculator extends GUI {
 
     private void reset() {
         buttonAvailable(true);
-        for (int i = 0; i < 10; i++) {
-            dec[i] = ""; symb[i] = "";
-        }
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 2; j++)
+                calc[i][j] = "";
         curr = 0;
         mathField.setText("");
     }
